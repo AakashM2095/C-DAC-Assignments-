2b25aad4056f25a96cb87fbf63357a98a4bca41f
AakashM2095 <113437293+AakashM2095@users.noreply.github.com>
2022-09-16 22:58:45 +0530

    Add files via upload

---
+++ AddWithout.java
@@ -0,0 +1,17 @@
+class AddWithout
+{
+	public static void main(String args[])
+	{
+		int sum = add(10,12);
+		System.out.println("The sum of value is : " +sum);
+	}
+	public static int add(int a,int b)
+	{
+		for(int i=1;i<=b;i++)
+		
+			a++;
+			return a;
+		
+		
+	}
+}
